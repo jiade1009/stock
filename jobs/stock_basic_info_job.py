7acf7724e0b548fdeb199497e72960388b7202e2
jiade1009 <jiade1009@163.com>
2022-10-11 16:17:04 +0800

    增加获取A股所有股票代码

--- jobs/stock_basic_info_job.py
+++ jobs/stock_basic_info_job.py
@@ -1,13 +1,19 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
-
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(BASE_DIR)
 
 """
 通过获取A股中所有股票的实时行情，整理出当前A股的股票代码
 """
-import libs.common as common
 import numpy as np
+import pandas as pd
+import datetime
+import traceback
 import akshare as ak
+import libs.common as common
 from log.logUtils import logger
 
 
@@ -58,49 +64,70 @@ def stock_a_filter_price(latest_price) -> bool:
         return True
 
 
-def load_a_stock(tmp_datetime):
+def load_a_stock():
     """
     更新A股股票信息
-    1.获取当前A股的行情信息
+    1.获取当前A股的行情信息：上证、深证、北证
     2.过滤不符合条件的A股股票
-    :param tmp_datetime:
+    3.表清空
+    4.插入新的数据集合
     :return:
     """
-    datetime_str = tmp_datetime.strftime("%Y-%m-%d")
-    datetime_int = tmp_datetime.strftime("%Y%m%d")
-    logger.info("load A stock, date: %s", str(datetime_int))
-
+    logger.info("load A stock")
+    datetime_int = (datetime.datetime.now()).strftime("%Y%m%d")
     try:
-        stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
-        # columns内容：['序号', '代码', '名称', '最新价', '涨跌幅', '涨跌额', '成交量', '成交额', '振幅', '最高', '最低',
-        #        '今开', '昨收', '量比', '换手率', '市盈率-动态', '市净率', '总市值', '流通市值', '涨速', '5分钟涨跌',
-        #        '60日涨跌幅', '年初至今涨跌幅']
-        stock_zh_a_spot_em_df.columns = ['index', 'code', 'name', 'latest_price', 'quote_change', 'ups_downs', 'volume',
-                                         'turnover', 'amplitude', 'high', 'low', 'open', 'closed', 'quantity_ratio',
-                                         'turnover_rate', 'pe_dynamic', 'pb', 'total_market', 'circulate_market',
-                                         'growth-rate', 'five_minute', 'sixty-up_down', 'to_date_up_down']
+        # 上证_A股 ['证券代码', '证券简称', '扩位证券简称', '上市日期']
+        stock_info_sh_A_name_code_df = ak.stock_info_sh_name_code(indicator="主板A股")
+        # print("stock_info_sh_A_name_code_df: %s %d" % (stock_info_sh_A_name_code_df.columns,
+        #                                                len(stock_info_sh_A_name_code_df)))
+        stock_info_sh_A_name_code_df = stock_info_sh_A_name_code_df.drop(columns=['扩位证券简称'])
+        stock_info_sh_A_name_code_df.columns = ['code', 'name', 'date_publish']
+        # print(stock_info_sh_A_name_code_df.head())
 
-        stock_zh_a_spot_em_df = stock_zh_a_spot_em_df.loc[stock_zh_a_spot_em_df["code"].apply(stock_a)]. \
-            loc[stock_zh_a_spot_em_df["name"].apply(stock_a_filter_st)]. \
-            loc[stock_zh_a_spot_em_df["latest_price"].apply(stock_a_filter_price)]
-        # 保留指定的列
-        stock_zh_a_spot_em_df = stock_zh_a_spot_em_df[['code', 'name', 'total_market', 'circulate_market']]
-        stock_zh_a_spot_em_df['date'] = datetime_int  # 修改时间成为int类型。
+        # 上证_科创 ['证券代码', '证券简称', '扩位证券简称', '上市日期']
+        stock_info_sh_KC_name_code_df = ak.stock_info_sh_name_code(indicator="科创板")
+        # print("stock_info_sh_KC_name_code_df: %s %d" % (stock_info_sh_KC_name_code_df.columns,
+        #                                                 len(stock_info_sh_KC_name_code_df)))
+        stock_info_sh_KC_name_code_df = stock_info_sh_KC_name_code_df.drop(columns=['扩位证券简称'])
+        stock_info_sh_KC_name_code_df.columns = ['code', 'name', 'date_publish']
+        # print(stock_info_sh_KC_name_code_df.head())
 
-        # 删除老数据。
+        # 深证 ['板块', 'A股代码', 'A股简称', 'A股上市日期', 'A股总股本', 'A股流通股本', '所属行业']
+        stock_info_sz_A_name_code_df = ak.stock_info_sz_name_code(indicator="A股列表")
+        # print("stock_info_sz_A_name_code_df: %s %d" % (stock_info_sz_A_name_code_df.columns,
+        #                                                len(stock_info_sz_A_name_code_df)))
+        stock_info_sz_A_name_code_df = stock_info_sz_A_name_code_df.drop(columns=['板块', 'A股总股本', 'A股流通股本', '所属行业'])
+        stock_info_sz_A_name_code_df.columns = ['code', 'name', 'date_publish']
+        # print(stock_info_sz_A_name_code_df.head())
+
+        # 北证 ['证券代码', '证券简称', '总股本', '流通股本', '上市日期', '所属行业', '地区', '报告日期']
+        stock_info_bj_name_code_df = ak.stock_info_bj_name_code()
+        # print("stock_info_bj_name_code_df: %s %d" % (stock_info_bj_name_code_df.columns,
+        #                                              len(stock_info_bj_name_code_df)))
+        stock_info_bj_name_code_df = stock_info_bj_name_code_df.drop(columns=['总股本', '流通股本', '所属行业', '地区', '报告日期'])
+        stock_info_bj_name_code_df.columns = ['code', 'name', 'date_publish']
+        # print(stock_info_bj_name_code_df.head())
+
+        stock_concat_df = pd.concat([stock_info_sh_A_name_code_df, stock_info_sh_KC_name_code_df,
+                                     stock_info_sz_A_name_code_df, stock_info_bj_name_code_df], ignore_index=True)
+        # print("stock_concat_df: %s %d" % (stock_concat_df.columns, len(stock_concat_df)))
+        # 移除ST股票
+        stock_concat_df = stock_concat_df.loc[stock_concat_df["name"].apply(stock_a_filter_st)]
+        stock_concat_df['date_publish'] = stock_concat_df['date_publish'].apply(lambda x: str(x).replace("-", ""))
+        # print(stock_concat_df.head())
+
+        # 删除老数据
         del_sql = " DELETE FROM `stock_basic_info` "
         common.insert(del_sql)
-        stock_zh_a_spot_em_df.set_index('code', inplace=True)
-        # stock_zh_a_spot_em_df.drop('index', axis=1, inplace=True)
-        print(stock_zh_a_spot_em_df.head(5))
-        common.insert_db(stock_zh_a_spot_em_df, "stock_basic_info", True, "`date`,`code`")
+        stock_concat_df.set_index('code', inplace=True)
+        # stock_concat_df.drop('index', axis=1, inplace=True)
+
+        #插入新数据
+        common.insert_db(stock_concat_df, "stock_basic_info", True, "`code`")
     except Exception as e:
         logger.error('错误明细是：%s %s', e.__class__.__name__, e)
+        traceback.print_exc()
 
-
-# main函数入口
-# 3种传递参数格式：
-#
 """
 main函数入口
 3种传递参数格式：
@@ -112,6 +139,6 @@ if __name__ == '__main__':
     # 执行数据初始化。
     # 使用方法传递。
     print("------------load stock_basic_info run ---------------")
-    dt = common.run_with_args(load_a_stock)
+    load_a_stock()
 
 print("............... run .........")
